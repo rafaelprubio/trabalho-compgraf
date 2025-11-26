2458e04a3c85117230af6448a4ba6d65cd300f58
rafaelprubio <rafael.p.rubio@unesp.br>
2025-11-26 01:22:09 -0300

    room 2
    criação inicial da sala 2

---
+++ Trabalho Computação Gráfica/Assets/Scripts/BackgroundMusic.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+public class BackgroundMusic : MonoBehaviour
+{
+    public static BackgroundMusic Instance;
+
+    void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+}
